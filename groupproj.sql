84be5e78b4ef04cab116c8a5f92ef24d6c61796b
Jason Dinh <47726701+jDinhBC@users.noreply.github.com>
2021-04-21 16:47:57 -0400

    file upload

---
+++ groupproj.sql
@@ -0,0 +1,23 @@
+CREATE TABLE season (
+ID int autoincrement primary key,
+week int
+
+
+)
+
+
+CREATE TABLE team(
+ID int autoincrement primary key,
+team int,
+
+)
+
+CREATE TABLE game(
+ID int autoincrement primary key,
+home FOREIGN KEY,
+visit FOREIGN KEY,
+home_score int,
+visit_score int,
+winner VARCHAR,
+)
+
