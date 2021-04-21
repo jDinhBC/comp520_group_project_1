1657230544e72698d50bb6ef97e7ec8c7c6c9952
Jason Dinh <kyuteec@gmail.com>
2021-04-21 19:04:46 -0400

    syntax bug fixes

--- ddl.sql
+++ ddl.sql
@@ -4,28 +4,29 @@
 */
 
 DROP DATABASE IF EXISTS comp520project1;
-CREATE DATABASE brokerage;
-USE brokerage;
+CREATE DATABASE comp520project1;
+USE comp520project1;
 
 CREATE TABLE seasons (
-SEASON_ID int NOT NULL autoincrement,
+SEASON_ID int NOT NULL AUTO_INCREMENT,
 year int,
-PRIMARY KEY (ID)
+PRIMARY KEY (SEASON_ID)
 );
 
 
 CREATE TABLE teams (
-TEAM_ID int NOT NULL autoincrement,
-team_name UNIQUE VARCHAR(255),
+TEAM_ID int NOT NULL AUTO_INCREMENT,
+team_name VARCHAR(255),
 team_city VARCHAR(255),
 home_stadium VARCHAR(255),
 team_nickname VARCHAR(255),
 team_owner VARCHAR(255),
+UNIQUE (team_name),
 PRIMARY KEY (TEAM_ID)
 );
 
 CREATE TABLE games (
-GAME_ID int NOT NULL autoincrement,
+GAME_ID int NOT NULL AUTO_INCREMENT,
 season int,
 home_team int,
 visiting_team int,
@@ -33,9 +34,9 @@ home_score int,
 visit_score int,
 game_date VARCHAR(255),
 game_stadium VARCHAR(255),
-PRIMARY KEY (ID),
+PRIMARY KEY (GAME_ID),
 FOREIGN KEY (season) REFERENCES seasons(SEASON_ID),
-FOREIGN KEY (home) REFERENCES teams(TEAM_ID),
-FOREIGN KEY (visit) REFERENCES teams(TEAM_ID)
+FOREIGN KEY (home_team) REFERENCES teams(TEAM_ID),
+FOREIGN KEY (visiting_team) REFERENCES teams(TEAM_ID)
 );
 
