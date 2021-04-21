ac68af1ead794a719c5018435fae8b198364cd49
Jason Dinh <kyuteec@gmail.com>
2021-04-21 17:47:30 -0400

    Completed sql table creation

--- ddl.sql
+++ ddl.sql
@@ -7,30 +7,35 @@ DROP DATABASE IF EXISTS comp520project1;
 CREATE DATABASE brokerage;
 USE brokerage;
 
-CREATE TABLE season (
-ID int autoincrement,
-year VARCHAR(255),
-week int,
+CREATE TABLE seasons (
+SEASON_ID int autoincrement,
+year int,
 PRIMARY KEY (ID)
 );
 
 
-CREATE TABLE team(
-TEAM_ID int autoincrement primary key,
+CREATE TABLE teams (
+TEAM_ID int NOT NULL autoincrement,
 team_name VARCHAR(255),
 team_city VARCHAR(255),
-home_stadium VARCHAR(255)
-
+home_stadium VARCHAR(255),
+nickname VARCHAR(255),
+owner VARCHAR(255),
+PRIMARY KEY (TEAM_ID)
 );
 
-CREATE TABLE game(
+CREATE TABLE games (
 GAME_ID int NOT NULL autoincrement,
+season int,
 home VARCHAR(255),
 visit VARCHAR(255),
 home_score int,
 visit_score int,
-winner VARCHAR,
+game_date VARCHAR(255),
+stadium VARCHAR(255),
 PRIMARY KEY (ID),
-FOREIGN KEY (ID) REFERENCES season(week)
+FOREIGN KEY (season) REFERENCES seasons(SEASON_ID),
+FOREIGN KEY (home) REFERENCES teams(TEAM_ID),
+FOREIGN KEY (visit) REFERENCES teams(TEAM_ID)
 );
 
